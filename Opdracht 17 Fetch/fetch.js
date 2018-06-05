7a6a89881e6d17b64ca16d0b68cbe2be90d8cc6a
psycoholic <katana_cs@hotmail.com>
2018-06-05 12:03:00 +0200

    Upload script 5-6-2018
    Opzet voor fetch opdracht

---
+++ Opdracht 17 Fetch/fetch.js
@@ -0,0 +1,65 @@
+// fetch('http://webtest.multiverseworks.com/triple-triad/game/cards?x=2&y=1&card=3').then(resp => resp.json(resp)).then(json => console.log(json));
+
+function getSetup() {
+    fetch('http://webtest.multiverseworks.com/triple-triad/game/setup', {
+        header: {'Content-Type': 'application/x-www-form-urlencoded'},
+    })
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+}
+
+function getCards() {
+    fetch('http://webtest.multiverseworks.com/triple-triad/game/cards', {
+        header: {'Content-Type': 'application/x-www-form-urlencoded'},
+    })
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+    .catch(error => console.error('Error:', error))
+}
+
+function getBoard(){
+    fetch('http://webtest.multiverseworks.com/triple-triad/game/board', {
+        header: {'Content-Type': 'application/x-www-form-urlencoded'},
+    })
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+    .catch(error => console.error('Error:', error))
+}
+
+function tossCoin(){
+    fetch('http://webtest.multiverseworks.com/triple-triad/game/toss-coin', {
+        header: {'Content-Type': 'application/x-www-form-urlencoded'},
+    })
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+    .catch(error => console.error('Error:', error))
+}
+
+function doCpu(){
+    fetch('http://webtest.multiverseworks.com/triple-triad/game/do-cpu', {
+        header: {'Content-Type': 'application/x-www-form-urlencoded'},
+    })
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+    .catch(error => console.error('Error:', error))
+}
+
+var data = {x: "1", y: "2"}
+
+function doPlayer(){
+    fetch('http://webtest.multiverseworks.com/triple-triad/game/do-player', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        header: {'Content-Type': 'application/x-www-form-urlencoded'},
+    })
+    .then(resp => resp.json())
+    .then(json => console.log(data))
+    .catch(error => console.error('Error:', error))
+}
+
+getSetup();
+getCards();
+getBoard();
+tossCoin();
+doCpu();
+doPlayer();
